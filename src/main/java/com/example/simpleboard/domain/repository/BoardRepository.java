ad0d8b0f77f5bdc004c51b4393e402209f8d07cf
esoh1234 <eunseokoh@safety-as.com>
2021-09-06 12:53:03 +0900

    0906Noon

--- src/main/java/com/example/simpleboard/domain/repository/BoardRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.simpleboard.domain.repository;
-
-import com.example.simpleboard.domain.entity.BoardEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BoardRepository extends JpaRepository<BoardEntity, Long> {
-}
